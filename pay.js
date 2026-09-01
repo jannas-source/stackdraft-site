a8ff9719df31c60c3db531ca6a02b0fc629ae02e
Autonome <autonome@7sense.net>
2026-09-01 10:31:36 -0400

    Stackdraft public site

---
+++ pay.js
@@ -0,0 +1,7 @@
+window.STACKDRAFT_PAY={
+  "install": "https://buy.stripe.com/fZudRbalc1aV5h27oabEA00",
+  "pack": "https://buy.stripe.com/6oU5kFctk5rb6l6aAmbEA01",
+  "scorecard": "https://buy.stripe.com/bJe7sN0KC4n78teeQCbEA02",
+  "prompts": "https://buy.stripe.com/28EcN7bpg5rb6l64bYbEA03",
+  "retainer": "https://buy.stripe.com/dRmbJ3ctkcTD4cYfUGbEA04"
+};
